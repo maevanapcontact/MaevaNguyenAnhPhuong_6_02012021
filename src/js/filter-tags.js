6d3e4503103f217294c885b6855d8c37a80f0126
maevanapcontact <maevanap.contact@gmail.com>
2021-01-08 16:46:08 +0100

    [#9] feat: add selected class on tags

--- src/js/filter-tags.js
+++ src/js/filter-tags.js
@@ -1,4 +1,5 @@
 import data from './data';
+import { createPhotographerElt } from './photographer-elt';
 
 /** Filter tags */
 const filterTags = () => {
@@ -10,7 +11,9 @@ const filterTags = () => {
   for (let i = 0; i < childrenElts.length; i++) {
     if (childrenElts[i].className == 'selected') {
       for (let j = 0; j < data.photographers.length; j++) {
-        if (data.photographers[j].tags.includes(childrenElts[i].lastChild.textContent.toLowerCase())) photographersToDisplay.push(data.photographers[j]);
+        if (data.photographers[j].tags.includes(childrenElts[i].lastChild.textContent.toLowerCase()) &&
+          !photographersToDisplay.includes(data.photographers[j])
+        ) photographersToDisplay.push(data.photographers[j]);
       }
     }
   }
@@ -19,4 +22,18 @@ const filterTags = () => {
   return photographersToDisplay;
 }
 
-export { filterTags }
+/** Toggle filter */
+const toggleFilter = (e) => {
+  const photographersSection = document.getElementById('photographers');
+
+  if (e.target.tagName === 'SPAN') e.target.parentNode.classList.toggle('selected');
+  if (e.target.tagName === 'LI') e.target.classList.toggle('selected');
+
+  let photographerToDisplay = filterTags();
+  photographersSection.innerHTML = '';
+  for (let i = 0; i < photographerToDisplay.length; i++) {
+    photographersSection.appendChild(createPhotographerElt(photographerToDisplay[i]));
+  }
+}
+
+export { filterTags, toggleFilter }
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import '../scss/main.scss';
 import { createPhotographerElt } from './photographer-elt';
 import { createPhotographerPage } from './photographer-page';
-import { filterTags } from './filter-tags';
+import { filterTags, toggleFilter } from './filter-tags';
 
 const photographersSection = document.getElementById('photographers');
 
 if (document.location.pathname.includes('index.html')) {
+  const tagsElt = document.getElementById('tags');
+  tagsElt.addEventListener('click', toggleFilter);
+
   let photographerToDisplay = filterTags();
   for (let i = 0; i < photographerToDisplay.length; i++) {
     photographersSection.appendChild(createPhotographerElt(photographerToDisplay[i]));
