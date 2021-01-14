50673b9e3f2304ac5cfff5ce1d456fc18cdcc45a
maevanapcontact <maevanap.contact@gmail.com>
2021-01-14 18:41:32 +0100

    [#9] feat: filter logic

--- src/js/photographer-page.js
+++ src/js/photographer-page.js
@@ -68,7 +68,27 @@ const fillPhotographerHeader = () => {
  */
 const createPhotographerWorksSection = () => {
   const worksElts = document.getElementById('works-elts');
-  photographerWorks.forEach(work => worksElts.appendChild(createWorkElt(work)));
+  const sortedWorks = sortPhotographers('popularite');
+  sortedWorks.forEach(work => worksElts.appendChild(createWorkElt(work)));
+}
+
+/**
+ * Sort the works array based on the filter type
+ *
+ * @return {array} Filtered array
+ */
+const sortPhotographers = type => {
+  if (type === 'popularite') return photographerWorks.sort((a, b) => b.likes - a.likes);
+  if (type === 'date') return photographerWorks.sort((a, b) => new Date(b.date) - new Date(a.date));
+  if (type === 'titre') {
+    return photographerWorks.sort((a, b) => {
+      const titleA = a.alt.toUpperCase();
+      const titleB = b.alt.toUpperCase();
+      if (titleA < titleB) return -1;
+      if (titleA > titleB) return 1;
+      return 0;
+    })
+  }
 }
 
 /**
