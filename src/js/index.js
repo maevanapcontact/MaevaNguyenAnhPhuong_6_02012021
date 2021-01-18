3f0b33dc712763054e7df12aa104d7805b2486fd
maevanapcontact <maevanap.contact@gmail.com>
2021-01-18 08:02:17 +0100

    manage link to content display

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,31 @@
-import '../scss/main.scss';
-import { createHomepage } from './homepage';
-import { createPhotographerPage } from './photographer-page';
+import "../scss/main.scss";
+import { createHomepage } from "./homepage";
+import { createPhotographerPage } from "./photographer-page";
 
 const path = window.location.pathname;
 
 /**
  * Create correct page based on the pathname
  */
-if (path.includes('photographer-page.html')) createPhotographerPage();
-else createHomepage();
+if (path.includes("photographer-page.html")) createPhotographerPage();
+else {
+  createHomepage();
+  document.addEventListener("scroll", manageContentNav);
+}
+
+/**
+ * Display or hide link to main content
+ *
+ * @return  {void}
+ */
+function manageContentNav() {
+  const bannerElt = document.getElementById("header-banner");
+  const linkElt = document.getElementById("link-content");
+
+  if (window.scrollY >= bannerElt.offsetHeight - 20) {
+    linkElt.style.top = "6px";
+  }
+  if (window.scrollY < bannerElt.offsetHeight - 20) {
+    linkElt.style.top = "-100px";
+  }
+}
