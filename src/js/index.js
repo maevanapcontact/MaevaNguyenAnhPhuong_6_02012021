9ae45efb4c890e5da5d4f61d633ea63319a2d3a9
maevanapcontact <maevanap.contact@gmail.com>
2021-01-19 12:37:31 +0100

    fix: display link only on main page

--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,18 @@ import { createHomepage } from "./homepage";
 import { createPhotographerPage } from "./photographer-page";
 
 const path = window.location.pathname;
-const linkElt = document.getElementById("link-content");
 
 /**
  * Create correct page based on the pathname
  */
 if (path.includes("photographer-page.html")) createPhotographerPage();
 else {
+  const linkElt = document.getElementById("link-content");
   createHomepage();
   document.addEventListener("scroll", manageContentNav);
+  linkElt.addEventListener("focus", () => (linkElt.style.top = "6px"));
+  linkElt.addEventListener("blur", () => (linkElt.style.top = "-100px"));
 }
-
-/**
- * Display or hide link to main content based on focus event
- *
- */
-linkElt.addEventListener("focus", () => (linkElt.style.top = "6px"));
-linkElt.addEventListener("blur", () => (linkElt.style.top = "-100px"));
 
 /**
  * Display or hide link to main content based on croll event
@@ -27,6 +22,7 @@ linkElt.addEventListener("blur", () => (linkElt.style.top = "-100px"));
  * @return  {void}
  */
 function manageContentNav() {
+  const linkElt = document.getElementById("link-content");
   const bannerElt = document.getElementById("header-banner");
 
   if (window.scrollY >= bannerElt.offsetHeight - 20) {
