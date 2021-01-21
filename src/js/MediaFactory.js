c913f4b3a228a69913d3d0f79329330e18f4bb1b
maevanapcontact <maevanap.contact@gmail.com>
2021-01-21 11:22:33 +0100

    chore: clean code

--- src/js/MediaFactory.js
+++ src/js/MediaFactory.js
@@ -1,23 +1,33 @@
-import { createImgElt, createTextualElt } from './utils';
+import { createImgElt, createTextualElt } from "./utils";
 
 class ImageFactory {
   constructor(props) {
     this.src = props.image;
     this.alt = props.alt;
     this.id = props.id;
-    this.type = 'image';
+    this.type = "image";
   }
 
+  /**
+   * Create the DOM element for thumbnail - img
+   *
+   * @return  {object}  The created element
+   */
   createElt() {
     const elt = createImgElt(`images/${this.src}`, `${this.alt}, closeup view`);
-    elt.setAttribute('role', 'button');
+    elt.setAttribute("role", "button");
 
     return elt;
   }
 
+  /**
+   * Create the DOM element for full size - img
+   *
+   * @return  {object}  The created element
+   */
   createFullElt() {
     const elt = createImgElt(`images/${this.src}`, this.alt);
-    elt.setAttribute('id', 'current-media-lightbox');
+    elt.setAttribute("id", "current-media-lightbox");
 
     return elt;
   }
@@ -28,29 +38,46 @@ class VideoFactory {
     this.src = props.video;
     this.alt = props.alt;
     this.id = props.id;
-    this.type = 'video';
+    this.type = "video";
   }
 
+  /**
+   * Logic for DOM creation - video
+   *
+   * @param   {string}  text  The alt text to add
+   *
+   * @return  {object}        The DOM element created
+   */
   domCreation(text) {
-    let elt = createTextualElt('video', text, 'video-elt');
-    let srcElt = document.createElement('source');
-    srcElt.setAttribute('src', `videos/${this.src}`);
+    let elt = createTextualElt("video", text, "video-elt");
+    let srcElt = document.createElement("source");
+    srcElt.setAttribute("src", `videos/${this.src}`);
     elt.appendChild(srcElt);
 
     return elt;
   }
 
+  /**
+   * Create the DOM element for thumbnail - video
+   *
+   * @return  {object}  The created element
+   */
   createElt() {
     const elt = this.domCreation(`${this.alt}, closeup view`);
-    elt.setAttribute('role', 'button');
+    elt.setAttribute("role", "button");
 
     return elt;
   }
 
+  /**
+   * Create the DOM element for full size - video
+   *
+   * @return  {object}  The created element
+   */
   createFullElt() {
     let elt = this.domCreation(this.alt);
-    elt.setAttribute('controls', true);
-    elt.setAttribute('id', 'current-media-lightbox');
+    elt.setAttribute("controls", true);
+    elt.setAttribute("id", "current-media-lightbox");
 
     return elt;
   }
@@ -58,7 +85,7 @@ class VideoFactory {
 
 export default class MediaFactory {
   constructor(type, props) {
-    if (type === 'image') return new ImageFactory(props);
-    if (type === 'video') return new VideoFactory(props);
+    if (type === "image") return new ImageFactory(props);
+    if (type === "video") return new VideoFactory(props);
   }
 }
